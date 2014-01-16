027153f3e344f17e1d0744d0cb0fcf2cfbce1e98
justinkwak <justin.kwak@nitmus.com>
2014-01-16 17:55:19 +0900

    gag

--- kck333/application/controllers/test/testdb.php
+++ kck333/application/controllers/test/testdb.php
@@ -10,10 +10,10 @@ class Testdb extends CI_Controller
 	function index()
 	{
 		
-		echo random_string('alnum', 16);
+		echo random_string('alnum', 500);
 		
 		exit;
-		$data 		= array('char'=>'AAAA','varchar'=>'BBBB','text'=>'무궁화꽃이피었습니다.');
+		$data 		= array('char'=>random_string('alnum', 5),'varchar'=>random_string('alnum', 10),'text'=>random_string('alnum', 500));
 		
 		$insert_id	= $this->m_test->add('idx_test', $data);
 		
