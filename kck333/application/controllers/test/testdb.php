ddda3b997418e8cf9d7134b49149d7801cc34204
justinkwak <justin.kwak@nitmus.com>
2014-01-16 17:53:16 +0900

    44

--- kck333/application/controllers/test/testdb.php
+++ kck333/application/controllers/test/testdb.php
@@ -9,6 +9,10 @@ class Testdb extends CI_Controller
 	
 	function index()
 	{
+		
+		echo random_string('alnum', 16);
+		
+		exit;
 		$data 		= array('char'=>'AAAA','varchar'=>'BBBB','text'=>'무궁화꽃이피었습니다.');
 		
 		$insert_id	= $this->m_test->add('idx_test', $data);
